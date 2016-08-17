2fd572be8137902ad5acb3fb40d5819d2e5d30f5
Jonathan <jonacom@lissismore.com>
2016-08-17 17:11:33 -0400

    implement print list and insert

---
+++ python/linked_list.py
@@ -0,0 +1,42 @@
+
+# python linked list - basic CRUD
+
+# Create X
+# Read
+# Update
+# Destroy
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, data):
+        self.head = Node(data)
+
+    def print_list(self, current=None):
+        if current == None:
+            current = self.head
+
+        print current.data
+        if current.next:
+            self.print_list(current.next)
+
+    def insert(self, data, current=None):
+        if current == None:
+            current = self.head
+            
+        if current.next == None:
+            current.next = Node(data)
+        else:
+            self.insert(data, current.next)
+
+    def find(self, data, current):
+        if current == None:
+
+ll = LinkedList("first node")
+ll.insert("second node")
+ll.insert("third node")
+ll.print_list()
